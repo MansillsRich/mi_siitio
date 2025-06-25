d4cbfea2ab9ef4d5023c9df875e6ffc8d67bea2a
Richard <mansillasrich@gmail.com>
2025-06-25 16:25:57 -0400

    Primer commit - Proyecto mi sitio

---
+++ paginas/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def inicio(request):
+    return render(request, 'paginas/inicio.html')
+
+def acerca(request):
+    return render(request, 'paginas/acerca.html')
+
+def contacto(request):
+    return render(request, 'paginas/contacto.html')
